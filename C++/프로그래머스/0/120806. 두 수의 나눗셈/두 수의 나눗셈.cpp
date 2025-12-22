b9f1e3780fc4ea5df16e0719eb7b009746243c50
HaNong <kidsmon@naver.com>
2025-12-22 10:08:30 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    float result = ((float)num1 / (float)num2)*1000;
+    int answer = result;
+    return answer;
+}
