dd9a1e1d5b15161d2e86c2ca1aa62c9321befd98
HaNong <kidsmon@naver.com>
2025-12-23 09:34:30 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ C++/프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int solution(int n)
+{
+    int answer = 0;
+    string num = to_string(n);
+    for(int i = 0; i < num.length(); ++i) {
+        answer += (num[i] - '0');
+    }
+
+    return answer;
+}
