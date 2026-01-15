d987cce080424124df1934d279def422d018f2ad
HaNong <kidsmon@naver.com>
2026-01-15 09:38:52 +0900

    [level 1] Title: 2016년, Time: 0.02 ms, Memory: 4.18 MB -BaekjoonHub

--- C++/프로그래머스/1/12901. 2016년/2016년.cpp
+++ C++/프로그래머스/1/12901. 2016년/2016년.cpp
@@ -6,47 +6,16 @@ using namespace std;
 
 string solution(int a, int b) {
     string answer = "";
-    map<int, int> calender =
-    {{1, 31}, {2, 29}, {3, 31}, {4, 30}, {5, 31}, {6, 30}, {7, 31}, {8, 31}, {9, 30}, {10, 31}, {11, 30}, {12, 31}};
+    const vector<int> daysByMonth = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    const vector<string> daysName = {"THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"};
     int days = 0;
     
     for(size_t i = 1; i < a; ++i) {
-        days += calender[i];
+        days += daysByMonth[i];
     }
     days += b;
     
-    switch (days % 7) {
-        case 0:
-            answer = "THU";
-            break;
-            
-        case 1:
-            answer = "FRI";
-            break;
-                
-        case 2:
-            answer = "SAT";
-            break;
-            
-        case 3:
-            answer = "SUN";
-            break;
-            
-        case 4:
-            answer = "MON";
-            break;
-            
-        case 5:
-            answer = "TUE";
-            break;
-            
-        case 6:
-            answer = "WED";
-            break;
-            
-        default:
-            break;
-    }
+    answer = daysName[days % 7];
     
     
     return answer;
