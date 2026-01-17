1dfbdc5c702263ec9a0e76744b3dfaaf7488b7d9
HaNong <kidsmon@naver.com>
2026-01-17 11:49:21 +0900

    [level 1] Title: 과일 장수, Time: 29.67 ms, Memory: 34.6 MB -BaekjoonHub

--- C++/프로그래머스/1/135808. 과일 장수/README.md
+++ C++/프로그래머스/1/135808. 과일 장수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 과일 장수 - 135808 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/135808) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/135808#) 
 
 ### 성능 요약
 
-메모리: 34.6 MB, 시간: 23.77 ms
+메모리: 34.6 MB, 시간: 29.67 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 17일 11:32:56
+2026년 01월 17일 11:49:21
 
 ### 문제 설명
 
--- C++/프로그래머스/1/135808. 과일 장수/과일 장수.cpp
+++ C++/프로그래머스/1/135808. 과일 장수/과일 장수.cpp
@@ -1,3 +1,4 @@
+// 챗봇이 제안한 코드 개선사항
 #include <string>
 #include <vector>
 #include <algorithm>
@@ -6,21 +7,17 @@ using namespace std;
 
 int solution(int k, int m, vector<int> score) {
     int answer = 0;
-    vector<int> box;
-    box.reserve(m);
-    box.assign(m, 0);
     
+    // 1. 점수를 내림차순으로 정렬합니다. (큰 점수가 앞으로 오도록)
     sort(score.begin(), score.end(), greater<int>());
-    for(size_t i = 0; i < score.size() / m; i++) {
-        for(size_t j = 0; j < box.size(); j++){
-            box[j] = score[i*m + j];
-        }
-        
-        int minCost = *min_element(box.begin(), box.end());
-        answer += m * minCost;
+    
+    // 2. 상자를 만들 수 있는 만큼만 반복합니다.
+    // i는 각 상자에서 가장 낮은 점수를 가진 사과의 인덱스를 가리킵니다.
+    // 예: m=3이면 인덱스 2, 5, 8... 순서로 접근
+    for(int i = m - 1; i < score.size(); i += m) {
+        // 해당 상자의 최저 점수(score[i]) * 상자에 담긴 사과 개수(m)
+        answer += score[i] * m;
     }
-    
-    
     
     return answer;
 }
