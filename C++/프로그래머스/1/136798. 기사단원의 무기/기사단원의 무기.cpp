3ddc79992d4461bc1c5d3fbd6a905816106cb03a
HaNong <kidsmon@naver.com>
2026-01-21 16:52:11 +0900

    [level 1] Title: 기사단원의 무기, Time: 1.44 ms, Memory: 4.21 MB -BaekjoonHub

--- C++/프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cpp
+++ C++/프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cpp
@@ -1,3 +1,4 @@
+// 챗봇의 개선사항 + 주석 추가
 #include <string>
 #include <vector>
 
@@ -5,22 +6,29 @@ using namespace std;
 
 int solution(int number, int limit, int power) {
     int answer = 0;
+    
+    // 1. 약수의 개수를 저장할 배열(벡터) 선언
+    // 인덱스를 1부터 N까지 편하게 쓰기 위해 크기를 N + 1로 잡는다.
+    // 모든 값을 0으로 초기화.
     vector<int> divCount(number + 1, 0);
-    
-    for(size_t i = 1; i <= number; ++i) {
-        for(size_t j = i; j <= number; j += i) {
-            divCount[j]++;
+
+    // 2. 알고리즘 핵심: 배수를 이용한 카운팅
+    for (int i = 1; i <= number; ++i) {
+        // j는 i의 배수 (i, 2i, 3i ... N까지)
+        for (int j = i; j <= number; j += i) {
+            divCount[j]++; // j는 i를 약수로 가지므로 카운트 증가
         }
     }
-    
-    for(auto it = divCount.begin(); it != divCount.end(); ++it) {
-        if(*it > limit) {
+
+    // [철의 무게 계산하기]
+    // 0번 인덱스는 건너뛰고 1번부터 number까지 확인합니다.
+    for (int i = 1; i <= number; ++i) {
+        if (divCount[i] > limit) {
             answer += power;
-            continue;
+        } else {
+            answer += divCount[i];
         }
-        
-        answer += *it;
     }
-    
+
     return answer;
 }
