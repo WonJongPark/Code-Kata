1e0d8498e72f98ec929f586b476e8724330a7513
HaNong <kidsmon@naver.com>
2026-01-31 23:28:06 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.03 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ C++/프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<string> wallpaper) {
+    vector<int> answer;
+    
+    int lux = 60, luy = 60, rdx = -1, rdy = -1;
+    
+    for(int i = 0; i < wallpaper.size(); ++i) {
+        for(int j = 0; j < wallpaper[i].length(); ++j) {
+            if(wallpaper[i][j] == '#'){
+                lux = min(lux, i);
+                luy = min(luy, j);
+                rdx = max(rdx, i);
+                rdy = max(rdy, j);         
+            }
+        }
+    }
+    
+    answer.push_back(lux);
+    answer.push_back(luy);
+    answer.push_back(rdx + 1);
+    answer.push_back(rdy + 1);
+    
+    return answer;
+}
